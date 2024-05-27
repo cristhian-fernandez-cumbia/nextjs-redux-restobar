943da384c5591651269b5cec033f88795b1b1342
cfernandez29 <165212076+fernandezcobraperu@users.noreply.github.com>
2024-05-27 05:58:41 -0500

    add changes en pedido

--- src/container/order/OrderCreate.tsx
+++ src/container/order/OrderCreate.tsx
@@ -10,32 +10,40 @@ interface OrderCreateProps {
 
 const OrderCreate: React.FC<OrderCreateProps> = ({ onClose, idTable, priceTotal }) => {
   const [paymentOption, setPaymentOption] = useState<'tarjeta' | 'yape' | 'efectivo'>('efectivo');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOrderGenerated, setIsOrderGenerated] = useState(false);
 
   const handleConfirmation = () => {
-    // Obtiene las órdenes del localStorage
-    let ordenes: Orden[] = JSON.parse(localStorage.getItem('pedidos') || '[]');
+    setIsLoading(true);
     
-    // Filtra las órdenes que pertenecen al idTable actual
-    ordenes = ordenes.filter(orden => orden.idTable !== idTable);
-    
-    // Guarda las nuevas órdenes en el localStorage
-    localStorage.setItem('pedidos', JSON.stringify(ordenes));
+    // Simular un retraso de 3 segundos
+    setTimeout(() => {
+      // Obtiene las órdenes del localStorage
+      let ordenes: Orden[] = JSON.parse(localStorage.getItem('pedidos') || '[]');
+      
+      // Filtra las órdenes que pertenecen al idTable actual
+      ordenes = ordenes.filter(orden => orden.idTable !== idTable);
+      
+      // Guarda las nuevas órdenes en el localStorage
+      localStorage.setItem('pedidos', JSON.stringify(ordenes));
 
-    // Obtiene la variable attentionPeople del localStorage
-    let attentionPeople = JSON.parse(localStorage.getItem('attentionPeople') || '[]');
+      // Obtiene la variable attentionPeople del localStorage
+      let attentionPeople = JSON.parse(localStorage.getItem('attentionPeople') || '[]');
 
-    // Busca el objeto con el idTable actual y actualiza su numPersons a 0
-    attentionPeople = attentionPeople.map((item: any) => {
-      if (item.idTable === idTable) {
-        return { ...item, numPersons: 0 };
-      }
-      return item;
-    });
+      // Busca el objeto con el idTable actual y actualiza su numPersons a 0
+      attentionPeople = attentionPeople.map((item: any) => {
+        if (item.idTable === idTable) {
+          return { ...item, numPersons: 0 };
+        }
+        return item;
+      });
 
-    // Guarda la variable attentionPeople actualizada en el localStorage
-    localStorage.setItem('attentionPeople', JSON.stringify(attentionPeople));
-    
-    onClose();
+      // Guarda la variable attentionPeople actualizada en el localStorage
+      localStorage.setItem('attentionPeople', JSON.stringify(attentionPeople));
+      
+      setIsLoading(false);
+      setIsOrderGenerated(true);
+    }, 3000);
   };
 
   const handlePaymentOptionChange = (option: 'tarjeta' | 'yape' | 'efectivo') => {
@@ -55,6 +63,17 @@ const OrderCreate: React.FC<OrderCreateProps> = ({ onClose, idTable, priceTotal 
     }
     return '0% Comisión';
   };
+
+  if (isOrderGenerated) {
+    return (
+      <div className='flex flex-col mt-2'>
+        <h2 className='text-green-600 font-bold'>Pedido generado con éxito</h2>
+        <div className='flex justify-end mt-10'>
+          <Button className='bg-primary hover:bg-red-900 px-5 py-2 flex justify-center text-base font-semibold text-white rounded-lg' onClick={onClose}>Cerrar</Button>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className='flex flex-col mt-2'>
@@ -86,8 +105,20 @@ const OrderCreate: React.FC<OrderCreateProps> = ({ onClose, idTable, priceTotal 
         S/{priceTotal.toFixed(2)} + {getCommission()} = <span className='font-bold text-xl'>S/{getTotalPrice()}</span>
       </p>
       <div className='flex gap-2 justify-end mt-10'>
-        <Button className='bg-green-500 hover:bg-green-700 px-5 py-2  flex justify-center text-base font-semibold text-white rounded-lg' onClick={handleConfirmation}>Generar Pedido</Button>
-        <Button className='bg-primary hover:bg-red-900 px-5 py-2 w-12 flex justify-center text-base font-semibold text-white rounded-lg' onClick={onClose}>NO</Button>
+        <Button 
+          className='bg-green-500 hover:bg-green-700 px-5 py-2 flex justify-center text-base font-semibold text-white rounded-lg'
+          onClick={handleConfirmation}
+          // disabled={isLoading}
+        >
+          {isLoading ? 'Generando...' : 'Generar Pedido'}
+        </Button>
+        <Button 
+          className='bg-primary hover:bg-red-900 px-5 py-2 w-12 flex justify-center text-base font-semibold text-white rounded-lg' 
+          onClick={onClose}
+          // disabled={isLoading}
+        >
+          NO
+        </Button>
       </div>
     </div>
   );
